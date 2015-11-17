d312cd153f62533238db96850fb53f538ed6ffe9
David Laietta <davidjlaietta@gmail.com>
2015-11-16 22:44:25 -0500

    Now that we've got our own footer.php file, it'll override the footer.php file in the parent theme.enter We're using the after_setup_theme() action hook to setup our new theme, which is the first action hook that WordPress makes available to themes, which is whenwe want to make our changes.

--- functions.php
+++ functions.php
@@ -1,9 +1,14 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'tfc_wp_enqueue_scripts' );
-function tfc_wp_enqueue_scripts() {
+add_action( 'after_setup_theme', 'tfc_after_setup_theme', 11 );
 
+function tfc_after_setup_theme() {
+	register_nav_menu( 'footer_menu', __( 'Footer Menu', 'twentyfifteen-child' ) );
+}
+
+add_action( 'wp_enqueue_scripts', 'tfc_wp_enqueue_scripts' );
+
+function tfc_wp_enqueue_scripts() {
 	wp_enqueue_style( 'twentyfifteen-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'twentyfifteen-child-style', get_stylesheet_directory_uri() . '/style.css' );
-
 }
